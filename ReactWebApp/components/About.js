e4192fb830663713843b0b25a34530e6a1a160e7
Marc H. Weiner <mhweiner234@gmail.com>
2021-05-23 15:51:11 -0400

    tweak bio

--- ReactWebApp/components/About.js
+++ ReactWebApp/components/About.js
@@ -42,28 +42,14 @@ export default class About extends React.PureComponent {
             <img src="/static/images/profile2.jpg"/>
           </div>
           <div className={styles.text}>
-            <p>
-              I am a builder, UI designer, and space nerd. As a kid, I enjoyed
-              building custom high-powered model rockets, R/C airplanes, and programming.
-            </p>
-  
-            <p>
-              After studying Aerospace Engineering and Computer Science at the University at Buffalo, I helped launch a
-              handful of successful startups. While working at a health-tech startup, I realized
-              that there are mission-critical web-based applications, just like in aerospace. Ever since, I've
-              focused on ways to help improve reliability in software in other industries.
-            </p>
+            <p>I've always been passionate about engineering, science, and space exploration. Since I was very young, I had an insatiable curiosity for how things worked, and wanted to build things that could make the world a better place.</p>
+            <p>As a kid, I spent much of my time programming and building custom model rockets and R/C airplanes. When I was in second grade, I found an old junk computer and learned how to write programs for it using a book from my elementary school's library. At age fifteen, I started my own company building web applications for local businesses and non-profits. I started to become passionate about UI design and learn how the way we interact with technology is as pivotal to solving the problem as the software itself.</p>
+            <p>After studying Aerospace Engineering and Computer Science at the University at Buffalo, I co-founded a mission-driven health-tech startup set to improve patient care, leveraging my UI design and systems problem solving skills. NextEMR was the first UX-centric <a href="https://en.wikipedia.org/wiki/Electronic_health_record" target="_blank">EHR</a> application. I soon realized that there are safety-critical applications in healthcare, just like in aerospace. That company was acquired in 2009.</p>
             
-            <p>
-              My main interests are in systems design, reliability, scalability, process optimization, and user experience.
-            </p>
+            <p>Ever since, I've focused on ways to help improve HCI (Human Computer Interaction) and reliability in software in other industries.</p>
+            <p>My main interests are in systems engineering, reliability, scalability, process optimization, and user experience.</p>
+            <p>When I'm not hammering away at the keyboard or having existential thoughts over <a target='_blank' href="https://www.goodreads.com/author/quotes/45368.Kevlin_Henney">Kevlin Henney quotes</a>, you can find me racing down a mountain on my snowboard, <a target='_blank' href="https://24hourultraskate.com/">skating 100+ miles in 24 hours</a>, working on electronics, or flying airplanes.</p>
             
-            <p>
-              When I'm not hammering away at the keyboard or having existential thoughts over <a target='_blank' href="https://www.goodreads.com/author/quotes/45368.Kevlin_Henney">Kevlin Henney quotes</a>,
-              you can find me on the mountain, <a target='_blank' href="https://24hourultraskate.com/">skating 100+ miles in 24 hours</a>, working on electronics, or flying airplanes.
-            </p>
-            
-            <h2>Want to hire me? Get in touch.</h2>
             <ul className={styles.contact}>
               <li>
                 <a href="mailto:mhweiner234@gmail.com" target="_blank">
@@ -75,6 +61,12 @@ export default class About extends React.PureComponent {
                 <a href="http://linkedin.com/in/mhweiner" target="_blank">
                   <i className='fab fa-linkedin-in'/>
                   <p>LinkedIn</p>
+                </a>
+              </li>
+              <li>
+                <a href="http://mhweiner.medium.com" target="_blank">
+                  <i className='fab fa-medium'/>
+                  <p>Medium</p>
                 </a>
               </li>
               <li>
